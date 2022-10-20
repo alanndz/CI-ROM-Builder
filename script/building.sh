26a86b798a42392ebc05db8e64dc673fe46d6db5
AnGgIt88 <jarbull87@gmail.com>
2022-10-20 14:14:53 +0000

    ready

--- script/building.sh
+++ script/building.sh
@@ -14,5 +14,5 @@ which ccache
 ccache -M 10
 ccache -z
 command=$(tail $CIRRUS_WORKING_DIR/build.sh -n +$(expr $(grep '# build rom' $CIRRUS_WORKING_DIR/build.sh -n | cut -f1 -d:) - 1)| head -n -1 | grep -v '# end')
-bash -c "$command" |& tee -a $WORKDIR/rom/$name_rom/build.log || true & sleep 95m
+bash -c "$command" |& tee -a $WORKDIR/rom/$name_rom/build.log || true #& sleep 95m
 bash $CIRRUS_WORKING_DIR/script/check_build.sh
