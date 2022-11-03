85dcc95e236c18a80489162eef851868418c7918
AnGgIt88 <jarbull87@gmail.com>
2022-11-03 12:50:32 +0000

    Fix missing config

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-PE-plu
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source $CIRRUS_WORKING_DIR/script/configconfig
+source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 source build/envsetup.sh
