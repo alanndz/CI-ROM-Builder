60e165b186c330fbcae8d2751430d0f84c0891fc
FinixDev™ <jarbull87@gmail.com>
2022-11-12 21:06:02 +0700

    Change color txt for non user

--- rom/checks.sh
+++ rom/checks.sh
@@ -4,6 +4,10 @@ msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
 
+msg1() {
+    echo -e "\e[1;31m$*\e[0m"
+}
+
 if [[ "$CIRRUS_REPO_OWNER" == "NFS-Project" ]]; then
     msg $CIRRUS_REPO_OWNER Ok anda adalah bagian dari Team
 elif [[ "$CIRRUS_REPO_OWNER" == "NFS86" ]]; then
@@ -15,6 +19,6 @@ elif [[ "$CIRRUS_REPO_OWNER" == "c3eru" ]]; then
 elif [[ "$CIRRUS_REPO_OWNER" == "zacky46" ]]; then
     msg $CIRRUS_REPO_OWNER Ok anda adalah bagian dari Team
 else
-    echo $CIRRUS_REPO_OWNER maaf anda bukan bagian dari Team
+    msg1 $CIRRUS_REPO_OWNER maaf anda bukan bagian dari Team
     exit 1
 fi
