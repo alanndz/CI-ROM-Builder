c765afb1ec6d44364f1fb1a33b4f85499fa1ee26
FinixDev™ <jarbull87@gmail.com>
2022-11-12 19:33:41 +0700

    Fix duplicate send log

--- rom/check_build.sh
+++ rom/check_build.sh
@@ -6,24 +6,8 @@ msg() {
 
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
-a=$(grep 'FAILED:' $WORKDIR/rom/$name_rom/build.log -m1 || true)
-b=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
-if [[ $a == *'FAILED:'* ]]
-then
-cd $WORKDIR/rom/$name_rom
-echo ━━━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━━━
-msg ⛔ .....Building Failed..... ⛔
-echo ━━━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━━━
-curl -F document=@build.log "https://api.telegram.org/bot${TG_TOKEN}/sendDocument" \
-    -F chat_id="${TG_CHAT_ID}" \
-    -F "disable_web_page_preview=true" \
-    -F "parse_mode=html" \
-    -F caption="⛔${device} Build $name_rom Error⛔
-
-Mohon bersabar ini ujian, Kalao gk sabar ya banting aja HP nya awowok😅"
-curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendSticker" -d sticker="CAACAgQAAx0EabRMmQACAvhjEpueqrNRuGJo5vCfzrjjnFH1gAACagoAAtMOGVGNqOvAKmWo-h4E" -d chat_id="${TG_CHAT_ID}"
-fi
-if [[ $b == *'#### build completed successfully'* ]]
+a=$(grep '#### build completed successfully' $WORKDIR/rom/$name_rom/build.log -m1 || true)
+if [[ $a == *'#### build completed successfully'* ]]
   then
   echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
   msg ✅ Build is completed 100% ✅
