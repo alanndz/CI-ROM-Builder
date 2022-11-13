3d7640074c5da1abbfc22534f265565cda389ccb
FinixDev™ <jarbull87@gmail.com>
2022-11-13 21:40:07 +0700

    Test fix sepolicy

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-make xtended > reading & sleep 95m # Jangan di hapus text line (> reading)
+make xtended -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
