31a69157c23b483f12ce323751d2edadcb27d7c4
FinixDev™ <jarbull87@gmail.com>
2022-11-14 10:14:01 +0700

    Add cek swap apace

--- rom/checks.sh
+++ rom/checks.sh
@@ -8,6 +8,26 @@ msg1() {
     echo -e "\e[1;31m$*\e[0m"
 }
 
+msg2() {
+    echo -e "\e[1;33m$*\e[0m"
+}
+
+echo ""
+echo ""
+msg2 "Check storage"
+echo "==============================="
+df -h
+echo "==============================="
+echo ""
+echo ""
+msg2 "Check swap"
+echo "==============================="
+free -h
+echo "==============================="
+echo ""
+echo ""
+msg2 "User Info"
+echo "==============================="
 if [[ "$CIRRUS_REPO_OWNER" == "NFS-Project" ]]; then
     msg $CIRRUS_REPO_OWNER Ok anda adalah bagian dari Team
 elif [[ "$CIRRUS_REPO_OWNER" == "NFS86" ]]; then
@@ -22,3 +42,4 @@ else
     msg1 $CIRRUS_REPO_OWNER maaf anda bukan bagian dari Team
     exit 1
 fi
+echo "==============================="
