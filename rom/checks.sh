87558dcc3fa90fb1b3e07441736defacf723d4be
FinixDev™ <jarbull87@gmail.com>
2022-11-14 11:42:47 +0700

    Test swap

--- rom/checks.sh
+++ rom/checks.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# green
 msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
-
+# red
 msg1() {
     echo -e "\e[1;31m$*\e[0m"
 }
-
+# yellow
 msg2() {
     echo -e "\e[1;33m$*\e[0m"
 }
+# purple
+msg3() {
+    echo -e "\e[1;35m$*\e[0m"
+}
 
+sudo fallocate -l 12G /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=12582912
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
 echo ""
 echo ""
 msg2 "Check storage"
@@ -42,4 +52,14 @@ else
     msg1 $CIRRUS_REPO_OWNER maaf anda bukan bagian dari Team
     exit 1
 fi
+echo "==============================="
+echo ""
+echo ""
+echo "==============================="
+msg2 "🔐 Notes"
+msg3 "Untuk bisa menjadi bagian dari Team kami,
+Anda bisa hubungi admin dalam grup telegram kami pada link di bawah ini:"
+msg https://t.me/cri_grup
+echo ""
+msg2 "CR: NFS-Project"
 echo "==============================="
