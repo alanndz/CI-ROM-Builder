6039e3559d2c02ef8f96b904b018714da71b5ba9
FinixDev™ <jarbull87@gmail.com>
2022-11-15 21:08:11 +0700

    not god

--- rom/building.sh
+++ rom/building.sh
@@ -15,5 +15,5 @@ which ccache
 ccache -M 20
 ccache -z
 wget https://raw.githubusercontent.com/NFS-Project/cirrus-ci-skrip/master/rom/config -O $CIRRUS_WORKING_DIR/config
-timeout 95m bash -c "$command" || true
+bash -c "$command" || true
 bash -c "$(curl -sL https://raw.githubusercontent.com/NFS-Project/cirrus-ci-skrip/master/rom/check_build.sh)"
