d50048df46c35d2c4b61817db5b364c405ecf5a6
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-11-21 23:47:35 +0700

    Fix inline

--- rom/checks.sh
+++ rom/checks.sh
@@ -24,6 +24,7 @@ branch_name=$(grep init $CIRRUS_WORKING_DIR/build.sh | awk -F "-b " '{print $2}'
 echo ""
 msg2 "Cek Keamanan"
 echo "==============================="
+if
 AUTHOR=$(cd $CIRRUS_WORKING_DIR && git log --pretty=format:'%an' -1)
 for value in NFSDev™ FinixDev™ c3eru zacky IQ7
 do
