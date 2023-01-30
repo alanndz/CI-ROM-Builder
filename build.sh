904fe674ffb432777b466863b9e5f59c77bea1f3
ZenkaBestia <z.mail.6600@gmail.com>
2023-01-30 10:15:25 +0800

    ci: Zenka Nusantara

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u ssh://git@github.com/Nusantara-ROM/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://github.com/ZenkaBestia/device_xiaomi_lmi.git -b 13-evox device/xiaomi/lmi
-git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.git -b Tiramisu vendor/xiaomi/lmi
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
+git clone --depth=1 ssh://git@github.com/ZenkaBestia/device_xiaomi_lmi-a13.git -b 13-nad-sun device/xiaomi/lmi
+git clone --depth=1 ssh://git@github.com/ZenkaBestia/vendor_xiaomi_lmi-a13.git -b 13-nad-sun vendor/xiaomi/lmi
+git clone --depth=1 ssh://git@github.com/ZenkaBestia/kernel_xiaomi_lmi-a13.git -b zen_plus-13 kernel/xiaomi/lmi
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 . build/envsetup.sh
-lunch evolution_lmi-user
+lunch nad_lmi-user
 mkfifo reading
 tee "${BUILDLOG}" < reading &
 build_message "Building Started"
 progress &
-mka evolution -j16  > reading & sleep 95m
+mka nad -j16  > reading & sleep 95m
 
 retVal=$?
 timeEnd
