2482e439512633c241835a605fef688db8558526
alanndz <alanndz7@gmail.com>
2023-03-02 02:24:46 +0000

    cirrus: Update pat key
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BiancaProject/android.git -b 13 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-git clone --depth=1 https://alanndz:${PAT_KEY}@github.com/chaldeaprjkt/device_xiaomi_vayu -b 13-dudu device/xiaomi/vayu
-git clone --depth=1 https://alanndz:${PAT_KEY}@github.com/chaldeaprjkt/vendor_xiaomi_vayu -b 13-dudu vendor/xiaomi/vayu
+git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/device_xiaomi_vayu -b 13-dudu device/xiaomi/vayu
+git clone --depth=1 https://${PAT_KEY}@github.com/chaldeaprjkt/vendor_xiaomi_vayu -b 13-dudu vendor/xiaomi/vayu
 git clone --depth=1 https://github.com/aLnProject/device_xiaomi_vayu-kernel -b main device/xiaomi/vayu-kernel
-git clone --depth=1 https://alanndz:${PAT_KEY}@github.com/alanndz/certs -b main certs
+git clone --depth=1 https://${PAT_KEY}@github.com/alanndz/certs -b main certs
 
 # build rom
 source $CIRRUS_WORKING_DIR/script/config
