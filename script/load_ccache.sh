f5347dfe224bde71aa2459580781d3069de48a28
alanndz <alanndz7@gmail.com>
2023-03-05 16:02:21 +0000

    cirrus: Drive test
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- script/load_ccache.sh
+++ script/load_ccache.sh
@@ -5,6 +5,6 @@ mkdir -p ~/.config/rclone
 echo "$RCLONECONFIG_DRIVE" > ~/.config/rclone/rclone.conf
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 device=$(grep lunch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 3)
-rclone copy --drive-chunk-size 256M --stats 1s zendrive:ccache/$name_rom/ccache.tar.gz $WORKDIR -P
+rclone copy --drive-chunk-size 256M --stats 1s zendrive:ccache/ccache.tar.gz $WORKDIR -P
 tar xzf ccache.tar.gz
 rm -rf ccache.tar.gz
