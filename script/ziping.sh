4bfb0d4691d362aea4b46fc3e7d4826565397272
alanndz <alanndz7@gmail.com>
2023-03-05 12:54:02 +0000

    cirrus: Up same cc
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- script/ziping.sh
+++ script/ziping.sh
@@ -7,7 +7,7 @@ com ()
   tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 time com ccache 1
-rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz zendrive:ccache/$name_rom/$device -P
+rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz zendrive:ccache/$name_rom -P
 rm -rf ccache.tar.gz
 }
 
