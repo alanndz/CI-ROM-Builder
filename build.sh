3cb464551984ee3d8412465e21d233ac5c285ed0
alanndz <alanndz7@gmail.com>
2023-04-03 12:14:02 +0000

    build: Up to gof
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
 ./barom.sh --device whyred --lunch whyred-user
-./barom.sh -b -j 8 -u wet --timer 95m -- m dudu
+./barom.sh -b -j 8 -u gof --timer 95m -- m dudu
 
 # end
