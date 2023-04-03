f093faf029747d0c2377c392ee6b56b43c637531
alanndz <alanndz7@gmail.com>
2023-04-03 02:59:17 +0000

    build: Increase timer to 95m
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- build.sh
+++ build.sh
@@ -12,6 +12,6 @@ chmod +x barom.sh
 ./barom.sh -t ${TG_CHAT_ID} ${TG_TOKEN}
 ./barom.sh --ccache-dir "${WORKDIR}/ccache" --ccache-size 20G
 ./barom.sh --device vayu --lunch vayu-user
-./barom.sh -b -j 8 -u gof --timer 85m -- m dudu
+./barom.sh -b -j 8 -u gof --timer 95m -- m dudu
 
 # end
